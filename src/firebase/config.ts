ce910eea1cc11ddf7c6487b874b93252e4ab2314
Darakhshan waseem <bluechipsystemsfze@gmail.com>
2026-01-14 23:35:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2860611950-e038a",
+  "appId": "1:979095252413:web:b629025993bffd0d03b6d4",
+  "apiKey": "AIzaSyDCwvx9a-hfyKGR_srL-ht3CV0LINL3us0",
+  "authDomain": "studio-2860611950-e038a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "979095252413"
+};

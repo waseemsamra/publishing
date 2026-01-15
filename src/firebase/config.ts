f3686d46ed48efd20c7e9d4acab3d95254794dab
Darakhshan waseem <bluechipsystemsfze@gmail.com>
2026-01-15 15:55:33 +0000

    Try fixing this error: `Build Error: Export firebaseConfig doesn't exist

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, FirebaseOptions } from 'firebase/app';
 
-const firebaseConfig: FirebaseOptions = {
+export const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,

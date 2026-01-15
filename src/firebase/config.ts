1eeb5a37dad9a3d362a04cc6a8e05f6bca48eb3f
Darakhshan waseem <bluechipsystemsfze@gmail.com>
2026-01-15 16:16:48 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,16 +1,8 @@
-import { initializeApp, getApps, FirebaseOptions } from 'firebase/app';
-
-export const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+export const firebaseConfig = {
+  "projectId": "studio-2860611950-e038a",
+  "appId": "1:979095252413:web:b629025993bffd0d03b6d4",
+  "apiKey": "AIzaSyDCwvx9a-hfyKGR_srL-ht3CV0LINL3us0",
+  "authDomain": "studio-2860611950-e038a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "979095252413"
 };
-
-let firebase_app =
-  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
-
-export default firebase_app;

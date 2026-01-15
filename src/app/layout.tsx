ab867169cc0830235c75c03b2749e9a084fb2e27
Darakhshan waseem <bluechipsystemsfze@gmail.com>
2026-01-15 00:19:55 +0000

    only hero secion is aligned

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased overflow-x-hidden">
         <AuthContextProvider>
           <CartProvider>
             {children}

e5cc12a3a26c755330648b2247cb6e511b909b22
Darakhshan waseem <bluechipsystemsfze@gmail.com>
2026-01-15 00:24:33 +0000

    still same

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,13 +28,15 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased overflow-x-hidden">
-        <AuthContextProvider>
-          <CartProvider>
-            {children}
-            <Toaster />
-          </CartProvider>
-        </AuthContextProvider>
+      <body className="font-body antialiased">
+        <div className="overflow-x-hidden">
+          <AuthContextProvider>
+            <CartProvider>
+              {children}
+              <Toaster />
+            </CartProvider>
+          </AuthContextProvider>
+        </div>
       </body>
     </html>
   );

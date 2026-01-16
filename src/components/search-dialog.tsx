2bdb10e5bbdae8a2d85849df75af4dfdb7cfc9f9
Darakhshan waseem <bluechipsystemsfze@gmail.com>
2026-01-16 22:25:39 +0000

    its redirecting but search filter is not closing

--- src/components/search-dialog.tsx
+++ src/components/search-dialog.tsx
@@ -129,7 +129,9 @@ export function SearchDialog() {
             ) : filteredProducts.length > 0 ? (
                 <div className="p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
                     {filteredProducts.map(product => (
-                        <ProductCard key={product.id} product={product} />
+                        <div key={product.id} onClick={() => setOpen(false)}>
+                            <ProductCard product={product} />
+                        </div>
                     ))}
                 </div>
             ) : (
